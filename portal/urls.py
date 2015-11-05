18a3a273b373525f2dd999bbf4a5f0fce103a216
George Schneeloch <gschneel@mit.edu>
2015-11-05 17:57:18 -0500

    Added skeleton app

---
+++ portal/urls.py
@@ -0,0 +1,13 @@
+"""
+URLs for portal
+"""
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(
+        r'^$',
+        TemplateView.as_view(template_name='portal/index.html'),
+        name='portal_index'
+    ),
+]
