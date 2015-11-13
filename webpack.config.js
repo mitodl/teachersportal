2e3cfd7b0f0a385489d53082e4b6c2f74aa993da
Jamie Folsom <jamiefolsom@gmail.com>
2015-11-13 11:09:05 -0500

    Add bourbon and neat in a way that webpack can manage them.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var Promise = require('es6-promise').Promise;
 var path = require("path");
 var webpack = require('webpack');
 var BundleTracker = require('webpack-bundle-tracker');
+var NodeNeat = require("node-neat");
 
 module.exports = {
   context: __dirname,
@@ -31,6 +32,10 @@ module.exports = {
         loader: 'style!css!sass'
       }
     ]
+  },
+
+  sassLoader: {
+    includePaths: NodeNeat.includePaths
   },
 
   resolve: {
