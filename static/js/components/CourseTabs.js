2deef4f14fe6463d5700cc16411b05c72efe60d9
Jamie Folsom <jamiefolsom@gmail.com>
2015-11-23 14:52:25 -0500

    Add base tab UI shell.
    Removed conflict cruft I missed.
    
    Remove duplicated commit.

---
+++ static/js/components/CourseTabs.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
+// Required for material ui tabs
+import injectTapEventPlugin from "react-tap-event-plugin";
+injectTapEventPlugin();
+
+class CourseTabs extends React.Component {
+  render() {
+    return <div>
+        <Tabs id="tabs">
+            <Tab label="About" id="about" className="tab">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.
+            </Tab>
+            <Tab label="Content" id="content" className="tab">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Tab>
+            <Tab label="Reviews" id="reviews" className="tab">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.
+            </Tab>
+            <Tab label="Buy" id="buy" className="tab">
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Tab>
+        </Tabs>
+      </div>;
+  }
+}
+
+export default CourseTabs;
