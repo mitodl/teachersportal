62a1b76707cc0d4748d7dd28c73a8a19323dd356
George Schneeloch <gschneel@mit.edu>
2015-12-11 11:16:32 -0500

    Removed django-oscar-api

--- portal/urls.py
+++ portal/urls.py
@@ -4,14 +4,12 @@ URLs for portal
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-from oscarapi.app import application as oscar_api
 from oscar.app import application as oscar
 
 from portal.views import index_view, ccxcon_view, WebhooksCCXConView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^oscarapi/', include(oscar_api.urls)),
     url(r'^ccxcon/', ccxcon_view, name='ccxcon-api'),
     url(r'^api/v1/webhooks/ccxcon/$', WebhooksCCXConView.as_view(), name='webhooks-ccxcon'),
 ]
