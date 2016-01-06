150aa75e59b47c4140477657f8f7e2cac48150b1
Justin Abrahms <justin@abrah.ms>
2016-01-06 15:19:55 -0500

    Add debug logging to the hmac verification code

--- teachersportal/settings.py
+++ teachersportal/settings.py
@@ -280,6 +280,10 @@ LOGGING = {
             'handlers': ['console', 'syslog'],
             'level': LOG_LEVEL,
         },
+        'portal': {
+            'handlers': ['console', 'syslog'],
+            'level': LOG_LEVEL,
+        },
         'django': {
             'propagate': True,
             'level': DJANGO_LOG_LEVEL,
