a64709729f6c8621042e539e46fed85c33cc46c6
George Schneeloch <gschneel@mit.edu>
2016-01-21 16:23:03 -0500

    Implemented Course Card and Course Listing.

--- static/js/containers/App.js
+++ static/js/containers/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SnackbarWrapper from '../components/SnackbarWrapper';
-import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/lib/raised-button';
+import { connect } from 'react-redux';
 
 import {
   showLogin,
