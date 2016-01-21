9d40ea6f6610bdf82b17f273af8c66e784ee3b60
George Schneeloch <gschneel@mit.edu>
2016-01-21 14:13:01 -0500

    Changed 400 to 500 error to reflect internal problem

--- portal/views/checkout_api_test.py
+++ portal/views/checkout_api_test.py
@@ -240,7 +240,7 @@ class CheckoutAPITests(ProductTests):
                 })
             )
 
-        assert resp.status_code == 400, resp.content.decode('utf-8')
+        assert resp.status_code == 500, resp.content.decode('utf-8')
         assert Order.objects.count() - start == 1
         assert OrderLine.objects.count() - start_ol == 1
 
@@ -275,7 +275,7 @@ class CheckoutAPITests(ProductTests):
                 })
             )
 
-        assert resp.status_code == 400, resp.content.decode('utf-8')
+        assert resp.status_code == 500, resp.content.decode('utf-8')
         assert "Example Error" in resp.content.decode('utf-8')
         assert "Another Error" in resp.content.decode('utf-8')
 
