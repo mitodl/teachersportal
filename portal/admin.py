9008c827c4f3de2ca720bff81c1aef11f3aea410
Justin Abrahms <justin@abrah.ms>
2016-01-27 16:23:10 -0500

    Improve the look & feel of the admin.

--- portal/admin.py
+++ portal/admin.py
@@ -9,6 +9,20 @@ from django.contrib import admin
 from portal.models import Course, Module, BackingInstance
 
 
-admin.site.register(Course)
+class ModuleInline(admin.TabularInline):
+    "Module inline for editing on course admin"
+    model = Module
+    fields = ('title', 'price_without_tax')
+    extra = 0
+
+
+class CourseAdmin(admin.ModelAdmin):
+    "Course admin page"
+    inlines = [
+        ModuleInline
+    ]
+
+
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Module)
 admin.site.register(BackingInstance)
